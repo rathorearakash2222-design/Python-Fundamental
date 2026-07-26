f8e5c9389d79af7b6b38b0efa0140bc76b95711e
Akash Rathore <rathorearakash2222@gmail.com>
2026-07-26 20:54:48 +0530

    Assignment, Comparsion and Logical Operator

---
+++ Phases II/Assignment.py
@@ -0,0 +1,40 @@
+     # Basis Assignment
+x = int(input("Enter the Basic Assignment Operater :" ))
+print ("x"," = ", "x" ,":", x)
+
+
+        # Addition Assignment
+x = int(input("Enter the Addition Assignment Operater :" ))
+x += 5
+print ("x"," = ", "x" ,":", x)
+
+       # Subtraction Assignment
+x = int(input("Enter the Subtraction Assignment Operater :" )) 
+x -= 3
+print ("x"," = ", "x" ,":", x)
+
+       # Divide Asisgnment
+x = int(input("Enter the Divide Assignment Operater :", )) 
+x /= 5
+print ("x"," = ", "x" ,":", x) 
+
+        # Multiple Assisgnment 
+x = int(input("Enter the Multiple Assignment Operater :", )) 
+x *= 3
+print ("x"," = ", "x" ,":", x) 
+
+     # Modulus Asssignment 
+x = int(input("Enter the Modulus  Assignment Operater :", )) 
+x %= 3
+print (x)  
+
+  #   Floor Division Asignment 
+x = int(input("Enter the Floor Division Assignment Operater :", )) 
+x //= 8
+print (x)  
+
+#      # Power Asssisgnment 
+x = int(input("Enter the Power Assignment Operater :", )) 
+x **= 3
+print (x)     
+    
---
+++ Phases II/Comparison.py
@@ -0,0 +1,18 @@
+# Equal to ==
+x =12
+y =12
+print(x==y) 
+
+a =12
+b =13
+print(a!=b) 
+
+c = 12
+d =4
+print (c>d)
+print(c<d)
+
+r =12
+e =12
+print(r>=e) 
+print(r<=e)
